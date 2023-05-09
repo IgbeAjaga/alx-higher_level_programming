72aa300b413a6d447d3eaa9f1eda3861d09e3da8
IgbeAjaga <ajagaigbe144@gmail.com>
2023-05-09 16:56:31 -0700

    update to task 14

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-for i in range(122, 96, -1):
+for i in range(ord('z'), ord('A') - 1, -1):
     if i % 2 == 0:
-        letter = chr(i).upper()
+        print(chr(i), end='')
     else:
-        letter = chr(i)
-    print("{}".format(letter), end="")
+        print(chr(i).upper(), end='')
