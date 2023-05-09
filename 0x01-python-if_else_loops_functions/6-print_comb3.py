6557ff2a09fe208e757a448e22db6fb35c0b8034
IgbeAjaga <ajagaigbe144@gmail.com>
2023-05-09 13:51:52 -0700

    update to task 6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for i in range(0, 10):
+for i in range(1, 10):
     for j in range(i+1, 10):
-        print("{}, {}".format(i, j), end=", ")
+        print("{:02d}, {:02d}".format(i, j), end=", ")
