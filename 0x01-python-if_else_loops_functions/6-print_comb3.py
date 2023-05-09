96671c6ebf676836d4ad5cf017f0a93ecd249aca
IgbeAjaga <ajagaigbe144@gmail.com>
2023-05-09 13:46:17 -0700

    update to task 6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 for i in range(0, 10):
     for j in range(i+1, 10):
-        if digit1 == 8 and digit2 == 9:
-            print("{}{}".format(i, j))
-        else:
-            print("{}{}".format(i, j), end=", ")
+        print("{:02d}, {:02d}".format(i, j), end=", ")
