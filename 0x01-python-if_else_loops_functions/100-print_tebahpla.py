c7ed398e7f1fb543cbe60550587b1dc1ea45bdcd
IgbeAjaga <ajagaigbe144@gmail.com>
2023-05-09 17:00:44 -0700

    update to task 14

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for i in range(ord('z'), ord('A') - 1, -1):
     if i % 2 == 0:
-        print(chr(i), end='')
+        print("{}".format(chr(i)) end='')
     else:
         print(chr(i).upper(), end='')
