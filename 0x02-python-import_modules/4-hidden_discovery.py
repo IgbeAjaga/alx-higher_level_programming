d026e3de0f9f4b6d61612650f245be6774768d48
IgbeAjaga <ajagaigbe144@gmail.com>
2023-05-11 10:50:50 -0700

    4-hidden_discovery.py update

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-import sys
-sys.path.append('/path/to/module/https://github.com/holbertonschool/0x02.py/raw/master/hidden_4.pyc')
 import hidden_4
 
 for name in sorted(dir(hidden_4)):
