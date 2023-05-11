49797aa4fdbf16cc694784643f99e8969b151d12
IgbeAjaga <ajagaigbe144@gmail.com>
2023-05-11 10:53:15 -0700

    4-hidden_discovery.py update

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 import hidden_4
 
-for name in sorted(dir(hidden_4)):
-    if not name.startswith("__"):
-        print(name)
+names = dir(hidden_4)
+    for name in names:
+        if name[:2] != "__":
+            print(name)
