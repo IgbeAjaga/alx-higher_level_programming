956f2f6102c8db44eb0e4442c5030681d8085914
IgbeAjaga <ajagaigbe144@gmail.com>
2023-05-15 09:22:51 -0700

    Python and C

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,17 @@
+#include <Python.h>
+#include <listobject.h>
+
+void print_python_list_info(PyObject *p)
+{
+    int size, i;
+    PyObject *item;
+
+    size = PyList_Size(p);
+    printf("[*] Size of the Python List = %d\n", size);
+    printf("[*] Allocated = %d\n", ((PyListObject *)p)->allocated);
+
+    for (i = 0; i < size; i++) {
+        item = PyList_GetItem(p, i);
+        printf("Element %d: %s\n", i, Py_TYPE(item)->tp_name);
+    }
+}
