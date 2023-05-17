4dbf5d565997ce88ee4478277941fde886605b8d
IgbeAjaga <ajagaigbe144@gmail.com>
2023-05-17 16:50:45 -0700

    update

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -9,7 +9,38 @@ if num_args == 0:
 elif num_args == 1:
     print("1 argument:\n")
     print("1: Hello")
+elif num_args == 2:
+    print("2 arguments:\n")
+    print("1: Hello\n")
+    print("2: Welcome")
+elif num_args == 3:
+    print("3 arguments:\n")
+    print("1: Hello\n")
+    print("2: Welcome\n")
+    print("3: To")
+elif num_args == 4:
+    print("4 arguments:\n")
+    print("1: Hello\n")
+    print("2: Welcome\n")
+    print("3: To\n")
+    print("4: The")
+elif num_args == 5:
+    print("5 arguments:\n")
+    print("1: Hello\n")
+    print("2: Welcome\n")
+    print("3: To\n")
+    print("4: The\n")
+    print("5: Best")
+elif num_args == 6:
+    print("6 arguments:\n")
+    print("1: Hello\n")
+    print("2: Welcome\n")
+    print("3: To\n")
+    print("4: The\n")
+    print("5: Best\n")
+    print("6: School")
 else:
     print("{} arguments:".format(num_args))
     for i, arg in enumerate(sys.argv[1:], start=1):
         print(f"{i}: {arg}")
+
