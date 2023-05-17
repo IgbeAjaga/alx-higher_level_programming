fe70cf75c6bb787dc11ab5e596bd0aec86880a67
IgbeAjaga <ajagaigbe144@gmail.com>
2023-05-17 15:43:08 -0700

    cpython

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -1,33 +1,47 @@
 #include <Python.h>
 
+/**
+ * print_python_list - Prints list
+ *
+ * @p: Object
+ * Return: no return
+ */
 void print_python_list(PyObject *p)
 {
 	Py_ssize_t size, i;
 	PyObject *item;
+
 	if (!PyList_Check(p))
 	{
 		printf("[ERROR] Invalid Python list object\n");
 		return;
 	}
-	size = PyList_Size(p);
+	size = PyObject_Length(p);
 	printf("[*] Size of the Python List = %zd\n", size);
 	for (i = 0; i < size; i++)
 	{
 		item = PyList_GetItem(p, i);
-		printf("Element %zd: %s\n", i, Py_TYPE(item)->tp_name);
+		printf("Element %zd: %s\n", i, item->ob_type->tp_name);
 	}
 }
 
+/**
+ * print_python_bytes - Prints bytes
+ *
+ * @p: Object
+ * Return: no return
+ */
 void print_python_bytes(PyObject *p)
 {
 	Py_ssize_t size, i;
 	char *data;
+
 	if (!PyBytes_Check(p))
 	{
 		printf("[ERROR] Invalid Python bytes object\n");
 		return;
 	}
-	size = PyBytes_Size(p);
+	size = PyObject_Length(p);
 	data = PyBytes_AsString(p);
 	printf("[.] bytes object info\n");
 	printf("  - Length: %zd\n", size);
