287ef7afabb073f3fc1723037b71130526b99eb4
IgbeAjaga <ajagaigbe144@gmail.com>
2023-05-29 15:58:46 -0700

    update

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def magic_calculation(a, b):
     result = 0
+
     for i in range(1, 3):
         try:
             if i > a:
@@ -9,4 +10,6 @@ def magic_calculation(a, b):
         except:
             result = b + a
             break
-    return (result)
+
+    return result
+
