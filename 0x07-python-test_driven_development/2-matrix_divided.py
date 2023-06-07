cbe47edad79b3c6ada4138a68b9004f303e0c7aa
IgbeAjaga <ajagaigbe144@gmail.com>
2023-06-07 13:15:51 -0700

    update

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -16,10 +16,8 @@ def matrix_divided(matrix, div):
         TypeError: If div is not a number
         ZeroDivisionError: If div is equal to 0
     """
-    if not isinstance(matrix, list) or not all(isinstance(row, list)
-            for row in matrix):
-        raise TypeError("matrix must be a matrix 
-                (list of lists) of integers/floats")
+    if not isinstance(matrix, list) or not all(isinstance(row, list) for row in matrix):
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
 
     row_size = len(matrix[0])
     if not all(len(row) == row_size for row in matrix):
