f2d14711d3967df9b05bd71ec11a1e2f46665592
IgbeAjaga <ajagaigbe144@gmail.com>
2023-06-07 15:36:32 -0700

    update

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -105,13 +105,13 @@ def repetition_solve(board, row, queens, result):
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
-        print("Usage: nqueens N\n")
+        print("Usage: nqueens N")
         sys.exit(1)
     if sys.argv[1].isdigit() is False:
-        print("N must be a number\n")
+        print("N must be a number")
         sys.exit(1)
     if int(sys.argv[1]) < 4:
-        print("N must be at least 4\n")
+        print("N must be at least 4")
         sys.exit(1)
 
     board = initial_board(int(sys.argv[1]))
