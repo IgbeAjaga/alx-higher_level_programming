c82b8cabcde7f13c2c6b4e4036392c6932c491eb
IgbeAjaga <ajagaigbe144@gmail.com>
2023-06-07 12:29:22 -0700

    update

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def magic_string():
-    magic_string.count = getattr(magic_string, 'count', 0) + 1
-    return ("BestSchool, " * (magic_string.count - 1) + "BestSchool")
-
+    return ', '.join(['BestSchool'] * magic_string.counter)
+magic_string.counter = 0
