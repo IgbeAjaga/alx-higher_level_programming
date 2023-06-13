bc1dac3265f8b2d8098af08e2d6df3478fb2734a
IgbeAjaga <ajagaigbe144@gmail.com>
2023-06-13 15:45:30 -0700

    update

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
 import json
+"""returns an object represented by a JSON string"""
+
 
 def to_json_string(my_obj):
+    """
+    A function that returns an object (
+    Python data structure) represented by a JSON string
+    """
     return json.dumps(my_obj)
