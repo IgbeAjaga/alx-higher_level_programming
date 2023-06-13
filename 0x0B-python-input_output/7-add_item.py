5bf47ad0190bfc7b52886fde4f283c96fac1a0c2
IgbeAjaga <ajagaigbe144@gmail.com>
2023-06-13 16:12:48 -0700

    update

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
+"""
+adds all arguments to a Python list, and then save them to a file
+"""
 import sys
 from os.path import exists
 from json import load
 from json import dump
 from 5-save_to_json_file import save_to_json_file
 from 6-load_from_json_file import load_from_json_file
+
 
 filename = "add_item.json"
 
