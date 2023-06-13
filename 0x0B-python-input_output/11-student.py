f90f49b9c4601ce13119a117dbec1244cc3e2a62
IgbeAjaga <ajagaigbe144@gmail.com>
2023-06-13 16:50:15 -0700

    update

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+"""defines a student by: (based on 10-student.py)"""
+
 
 class Student:
     """Class representing a student"""
@@ -9,14 +11,6 @@ class Student:
         self.last_name = last_name
         self.age = age
 
-    def to_json(self, attrs=None):
+    def to_json(self):
         """Retrieve a dictionary representation of a Student instance"""
-        if attrs is None:
-            return self.__dict__
-        else:
-            return {attr: getattr(self, attr) for attr in attrs if hasattr(self, attr)}
-
-    def reload_from_json(self, json):
-        """Replace all attributes of the Student instance"""
-        for key, value in json.items():
-            setattr(self, key, value)
+        return self.__dict__
