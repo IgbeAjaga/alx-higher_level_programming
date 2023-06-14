0978968a539bf17c1dcc6d001bcd73343d5dfac0
IgbeAjaga <ajagaigbe144@gmail.com>
2023-06-13 17:36:15 -0700

    update

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -5,41 +5,55 @@ reads stdin line by line and computes metrics
 import sys
 
 
-def print_stats(total_size, status_codes):
-    """Prints the computed statistics"""
-    print("File size: {:d}".format(total_size))
+def print_metrics(file_size, status_codes):
+    """
+    printing the file
+    """
+    print("File size: {}".format(file_size))
     for code in sorted(status_codes.keys()):
         if status_codes[code] > 0:
-            print("{:s}: {:d}".format(code, status_codes[code]))
+            print("{}: {}".format(code, status_codes[code]))
 
-def parse_line(line):
-    """Parses a line and extracts relevant information"""
-    line_parts = line.split()
-    size = int(line_parts[-1])
-    code = line_parts[-2]
-    return size, code
+def parse_line(line, file_size, status_codes):
+    """
+    parsing the file
+    """
+    tokens = line.split()
+    if len(tokens) > 2:
+        file_size += int(tokens[-1])
+        status_code = tokens[-2]
+        if status_code in status_codes:
+            status_codes[status_code] += 1
+        else:
+            status_codes[status_code] = 1
+    return file_size, status_codes
 
-def compute_metrics():
-    """Reads stdin line by line and computes metrics"""
-    total_size = 0
-    status_codes = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0, "404": 0, "405": 0, "500": 0}
-    count = 0
+def main():
+    """
+    main codes
+    """
+    file_size = 0
+    status_codes = {
+        '200': 0,
+        '301': 0,
+        '400': 0,
+        '401': 0,
+        '403': 0,
+        '404': 0,
+        '405': 0,
+        '500': 0
+    }
+    line_count = 0
 
     try:
         for line in sys.stdin:
-            count += 1
-            size, code = parse_line(line)
-            total_size += size
-
-            if code in status_codes:
-                status_codes[code] += 1
-
-            if count % 10 == 0:
-                print_stats(total_size, status_codes)
-
+            line_count += 1
+            file_size, status_codes = parse_line(line, file_size, status_codes)
+            if line_count % 10 == 0:
+                print_metrics(file_size, status_codes)
     except KeyboardInterrupt:
-        print_stats(total_size, status_codes)
+        print_metrics(file_size, status_codes)
         raise
 
-compute_metrics()
-
+if __name__ == "__main__":
+    main()
