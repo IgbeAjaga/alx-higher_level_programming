de478defa84207ad7304098248695359ed229534
IgbeAjaga <ajagaigbe144@gmail.com>
2023-07-19 15:26:10 -0700

    update

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,10 +1,7 @@
 -- List shows with at least one genre linked
 
 -- Use the hbtn_0d_tvshows database
-USE hbtn_0d_tvshows;
-
 -- Query to list shows with at least one genre linked
-SELECT tv_shows.title, tv_show_genres.genre_id
-FROM tv_shows
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows
 JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
 ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
