07a826fb1648c1019192ac3f4ef02271257186df
IgbeAjaga <ajagaigbe144@gmail.com>
2023-07-19 16:27:11 -0700

    update

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,11 +1,12 @@
 -- List all shows from hbtn_0d_tvshows_rate by their rating
 
 -- Use the hbtn_0d_tvshows_rate database
-USE hbtn_0d_tvshows_rate;
-
 -- Query to list all shows and their rating sum
-SELECT tv_shows.title, SUM(rating) AS rating_sum
-FROM tv_shows
-JOIN tv_show_ratings ON tv_shows.id = tv_show_ratings.tv_show_id
+SELECT tv_shows.title, SUM(rating) AS rating_sum FROM tv_shows
+WHERE tv_shows.id NOT IN (
+      SELECT tv_shows.id FROM tv_shows
+      JOIN tv_show_ratings ON tv_shows.id=tv_show_ratings.tv_show_id
+      JOIN tv_show_ratings ON tv_genres.id=tv_show_ratings.tv_genre_id
+       )
 GROUP BY tv_shows.title
 ORDER BY rating_sum DESC;
