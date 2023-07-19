1217c706c2e27d8764673b02f2973c6c5ce24ea4
IgbeAjaga <ajagaigbe144@gmail.com>
2023-07-19 15:46:54 -0700

    update

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -1,11 +1,8 @@
 -- List all shows without a genre linked
 
 -- Use the hbtn_0d_tvshows database
-USE hbtn_0d_tvshows;
-
--- Query to list all shows without a genre linked
-SELECT tv_shows.title, tv_show_genres.genre_id
-FROM tv_shows
-LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
-WHERE tv_show_genres.genre_id IS NULL
-ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
+-- List all shows without a genre linked
+SELECT s.`title`, g.`genre_id` FROM `tv_shows` AS s
+LEFT JOIN `tv_show_genres` AS g ON s.`id` = g.`show_id`
+WHERE g.`genre_id` IS NULL
+ORDER BY s.`title`, g.`genre_id`;
