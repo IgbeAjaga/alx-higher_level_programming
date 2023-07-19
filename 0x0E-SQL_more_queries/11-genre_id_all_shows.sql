6eb89c2fadafddb7fda0d5bec0aa9b4171bc3126
IgbeAjaga <ajagaigbe144@gmail.com>
2023-07-19 15:42:43 -0700

    update

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,10 +1,6 @@
 -- List all shows with genre_id as NULL for shows without a genre
-
 -- Use the hbtn_0d_tvshows database
-USE hbtn_0d_tvshows;
-
--- Query to list all shows with genre_id as NULL for shows without a genre
-SELECT tv_shows.title, tv_show_genres.genre_id
-FROM tv_shows
-LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
-ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
+-- List all shows with genre_id as NULL for shows without a genre
+SELECT s.`title`, g.`genre_id` FROM `tv_shows` AS s
+LEFT JOIN `tv_show_genres` AS g ON s.`id` = g.`show_id`
+ORDER BY s.`title`, g.`genre_id`;
