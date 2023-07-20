627f9f2cc5a7ebdd3c97a3a3c72666a1e43b468f
IgbeAjaga <ajagaigbe144@gmail.com>
2023-07-20 07:46:39 -0700

    update

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,11 +1,12 @@
---  List all shows from hbtn_0d_tvshows_rate by their rating
+-- Task 19: Rotten tomatoes
 
 -- Use the hbtn_0d_tvshows_rate database
+USE hbtn_0d_tvshows_rate;
+
 -- Query to list all shows and their rating sum
-SELECT title, SUM(rating) AS rating_sum
-FROM tv_shows_rate
-LEFT JOIN tv_show_rating_sum ON id = tv_show_rating_sum.show_id
-LEFT JOIN tv_rating_sum ON tv_show_rating_sum.rating_sum_id = tv_rating_sum.id
-GROUP BY title
+SELECT tv_shows.title, SUM(rate) AS rating_sum
+FROM tv_shows
+JOIN tv_show_ratings ON tv_shows.id = tv_show_ratings.show_id
+GROUP BY tv_shows.title
 ORDER BY rating_sum DESC;
 
