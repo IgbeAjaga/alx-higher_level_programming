5796da3cd88d134b08ec2ea70ac10d7b243a9d71
IgbeAjaga <ajagaigbe144@gmail.com>
2023-07-19 17:10:38 -0700

    update

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,4 @@
--- List all privileges of user_0d_1 and user_0d_2 on your local server
+-- List all privileges of user_0d_1
+-- and user_0d_2 on your local server
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
