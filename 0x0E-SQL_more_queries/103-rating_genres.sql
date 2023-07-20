28d69f7b901cf459cc51796f87fc56d440d04f15
IgbeAjaga <ajagaigbe144@gmail.com>
2023-07-20 08:05:12 -0700

    update

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -6,7 +6,7 @@ USE hbtn_0d_tvshows_rate;
 -- Query to list all genres and their rating sum
 SELECT tv_genres.name, SUM(rating) AS rating_sum
 FROM tv_genres
-JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
-JOIN tv_show_ratings ON tv_show_genres.tv_show_id = tv_show_ratings.tv_show_id
+LEFT JOIN tv_show_genres ON id = tv_show_genres.genre_id
+LEFT JOIN tv_show_ratings ON tv_show_genres.tv_show_id = tv_show_ratings.tv_show_id
 GROUP BY tv_genres.name
 ORDER BY rating_sum DESC;
