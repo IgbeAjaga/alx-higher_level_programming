75e8389f5f9c8728bdae8f1b0aa8259e0e0529b6
IgbeAjaga <ajagaigbe144@gmail.com>
2023-07-20 08:35:59 -0700

    update

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -3,7 +3,7 @@
 -- Use the hbtn_0d_tvshows_rate database
 -- Query to list all shows and their rating sum
 SELECT `title`, SUM(`rate`) AS `rating_sum`
-FROM tv_shows AS tv
+FROM `tv_shows` AS tv
 INNER JOIN `tv_show_ratings` AS rt ON tv.`id` = rt.`show_id`
 GROUP BY `title`
 ORDER BY `rating_sum` DESC;
