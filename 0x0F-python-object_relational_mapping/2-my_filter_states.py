9caaaf97e0f700cc751e34c1e923d484b9060d50
IgbeAjaga <ajagaigbe144@gmail.com>
2023-08-17 13:38:56 -0700

    making files executable

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -4,14 +4,15 @@ import sys
 
 if __name__ == "__main__":
     # Check for the correct number of command-line arguments
-    if len(sys.argv) != 4:
-        print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
+    if len(sys.argv) != 5:
+        print("Usage: {} <username> <password> <database> <state_name>".format(sys.argv[0]))
         sys.exit(1)
 
     # Get command-line arguments
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
+    state_name = sys.argv[4]
 
     # Connect to the MySQL server
     db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database)
@@ -19,9 +20,9 @@ if __name__ == "__main__":
     # Create a cursor
     cursor = db.cursor()
 
-    # Execute SQL query to get states starting with 'N'
-    query = "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC"
-    cursor.execute(query)
+    # Execute SQL query to get states matching the user input
+    query = "SELECT * FROM states WHERE name LIKE %s ORDER BY id ASC"
+    cursor.execute(query, (state_name,))
 
     # Fetch all results
     results = cursor.fetchall()
