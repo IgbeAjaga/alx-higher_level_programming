e7fc00258de5ca7949b7804b6779a74a97a86a93
IgbeAjaga <ajagaigbe144@gmail.com>
2023-08-20 17:46:20 -0700

    update

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,36 +1,16 @@
 #!/usr/bin/python3
+""" takes in an argument and displays all values"""
 import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    # Check for the correct number of command-line arguments
-    if len(sys.argv) != 5:
-        print("Usage: {} <username> <password> <database> <state_name>".format(sys.argv[0]))
-        sys.exit(1)
-
-    # Get command-line arguments
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-    state_name = sys.argv[4]
-
-    # Connect to the MySQL server
-    db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database)
-
-    # Create a cursor
-    cursor = db.cursor()
-
-    # Execute SQL query to get states matching the user input
-    query = "SELECT * FROM states WHERE name LIKE %s ORDER BY id ASC"
-    cursor.execute(query, (state_name,))
-
-    # Fetch all results
-    results = cursor.fetchall()
-
-    # Display results
-    for row in results:
-        print(row)
-
-    # Close cursor and database connection
-    cursor.close()
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    curs = db.cursor()
+    curs.execute("SELECT * FROM states WHERE name LIKE BINARY '{}'"
+                .format(sys.argv[4]))
+    records = curs.fetchall()
+    for record in records:
+        print(record)
+    curs.close()
     db.close()
