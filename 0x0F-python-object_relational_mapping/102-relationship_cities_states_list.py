0da5e3b7f68f62dd530b4e2511f22e2e4dfc5cd8
IgbeAjaga <ajagaigbe144@gmail.com>
2023-08-20 18:53:58 -0700

    update

--- 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -4,34 +4,18 @@
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import relationship
 from relationship_state import Base, State
 from relationship_city import City
 import sys
 
 if __name__ == "__main__":
-    # Check for the correct number of command-line arguments
-    if len(sys.argv) != 4:
-        print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
-        sys.exit(1)
-
-    # Get command-line arguments
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-
-    # Create an engine
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(username, password, database), pool_pre_ping=True)
-
-    # Create a session
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-
-    # Retrieve all City objects and their corresponding State objects using the state relationship
-    cities = session.query(City).order_by(City.id).all()
-
-    # Display the results
-    for city in cities:
-        print("{}: {} -> {}".format(city.id, city.name, city.state.name))
-
-    # Close the session
-    session.close()
+    for instance in session.query(State).order_by(State.id):
+        for city_ins in instance.cities:
+            print(city_ins.id, city_ins.name, sep=": ", end="")
+            print(" -> " + instance.name)
