8d6f3699718c6ddad7572f8cbf1d63850b71cca6
IgbeAjaga <ajagaigbe144@gmail.com>
2023-08-20 19:08:45 -0700

    update

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -2,39 +2,19 @@
 """Creates the State “California” with the City “San Francisco”
 """
 
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from relationship_state import Base, State
-from relationship_city import City
-import sys
+from sqlalchemy import Column, Integer, String, MetaData
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
 
-if __name__ == "__main__":
-    # Check for the correct number of command-line arguments
-    if len(sys.argv) != 4:
-        print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
-        sys.exit(1)
+mymetadata = MetaData()
+Base = declarative_base(metadata=mymetadata)
 
-    # Get command-line arguments
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
 
-    # Create an engine
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(username, password, database), pool_pre_ping=True)
-
-    # Create a session
-    Session = sessionmaker(bind=engine)
-    session = Session()
-
-    # Create a new State and City
-    new_state = State(name="California")
-    new_city = City(name="San Francisco")
-    new_state.cities.append(new_city)
-    session.add(new_state)
-
-    # Commit the changes
-    session.commit()
-
-    # Close the session
-    session.close()
-
+class State(Base):
+    """
+    Class with id and name attributes of each state
+    """
+    __tablename__ = 'states'
+    id = Column(Integer, unique=True, nullable=False, primary_key=True)
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", backref="states")
