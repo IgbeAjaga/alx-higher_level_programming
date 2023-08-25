4e2267066f18a518bbb347fbaffc4eb043548714
IgbeAjaga <ajagaigbe144@gmail.com>
2023-08-25 14:22:10 -0700

    pycodestyle update

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,36 +1,28 @@
 #!/usr/bin/python3
 """
-Changes the name of a State object in the database hbtn_0e_6_usa
+Changes the name of a State object from the database hbtn_0e_6_usa
 """
 
+import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
-import sys
 
 if __name__ == "__main__":
-    # Check for the correct number of command-line arguments
-    if len(sys.argv) != 4:
-        print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
-        sys.exit(1)
-
-    # Get command-line arguments
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
 
-    # Create an engine
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(username, password, database), pool_pre_ping=True)
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(username, password, database),
+                           pool_pre_ping=True)
 
-    # Create a session
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    # Find the State object with id = 2 and update its name
     state_to_update = session.query(State).filter_by(id=2).first()
-    if state_to_update:
-        state_to_update.name = "New Mexico"
-        session.commit()
+    state_to_update.name = "New Mexico"
+    session.commit()
 
-    # Close the session
     session.close()
+
