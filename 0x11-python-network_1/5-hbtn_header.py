0950707a5eec151d407c5e9b2e5dfd6119636a63
IgbeAjaga <ajagaigbe144@gmail.com>
2023-09-01 15:47:52 -0700

    python - network

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,11 +1,21 @@
 #!/usr/bin/python3
+"""
+Displays the value of the X-Request-Id
+variable found in the header of the response using requests
+"""
 import requests
-import sys
+from sys import argv
+
 
 if __name__ == "__main__":
-    url = sys.argv[1]
-
-    response = requests.get(url)
-
-    x_request_id = response.headers.get('X-Request-Id')
-    print(x_request_id)
+    """
+    Takes in a URL, sends a request to the URL and displays
+    the value of the X-Request-Id variable found in the header
+    of the response using requests
+    """
+    call = requests.get(argv[1])
+    try:
+        call_id = call.headers['X-Request-Id']
+        print(call_id)
+    except:
+        pass
