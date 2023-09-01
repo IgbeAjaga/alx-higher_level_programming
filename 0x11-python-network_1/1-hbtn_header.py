03b0f79bea21fb6c10f671f3916752361742736c
IgbeAjaga <ajagaigbe144@gmail.com>
2023-09-01 15:22:18 -0700

    python - network

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
+"""
+takes in a URL, sends a request to the URL and displays the value of the
+X-Request-Id variable found in the header of the response.
+"""
 import urllib.request
-import sys
+from sys import argv
+
 
 if __name__ == "__main__":
-    if len(sys.argv) == 2:
-        url = sys.argv[1]
-
-        req = urllib.request.Request(url)
-        with urllib.request.urlopen(req) as response:
-            header_value = response.info().get('X-Request-Id')
-            print(header_value)
+    """takes in a URL, sends a request to the URL and displays
+    the value of the X-Request-Id variable found in the header
+    of the response"""
+    with urllib.request.urlopen(argv[1]) as response:
+        frontend_id = response.info().get('X-Request-Id')
+        print(frontend_id)
