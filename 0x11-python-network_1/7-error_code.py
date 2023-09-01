40503db0c1f092ec80a43ea7a5148dff3378a91b
IgbeAjaga <ajagaigbe144@gmail.com>
2023-09-01 16:08:48 -0700

    python - network

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,13 +1,20 @@
 #!/usr/bin/python3
+"""
+Takes in a URL, sends a request to the URL and displays the
+body of the response using requests
+"""
 import requests
-import sys
+from sys import argv
+
 
 if __name__ == "__main__":
-    url = sys.argv[1]
-
-    response = requests.get(url)
-
-    if response.status_code >= 400:
-        print(f"Error code: {response.status_code}")
+    """
+    Takes in a URL, sends a request to the URL and displays the
+    body of the response using requests
+    """
+    url = argv[1]
+    call = requests.get(url)
+    if call.status_code >= 400:
+        print("Error code: {}".format(call.status_code))
     else:
-        print(response.text)
+        print(call.text)
