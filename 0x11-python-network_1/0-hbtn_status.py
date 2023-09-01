199b135695b00a43639b8caeaae97fb09320c772
IgbeAjaga <ajagaigbe144@gmail.com>
2023-09-01 15:06:42 -0700

    python - network

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
-"""A Python script that fetches a URL"""
+"""a Python script that fetches a url"""
 import urllib.request
 
+
 if __name__ == "__main__":
-    # Fetch the URL
-    url = 'https://alx-intranet.hbtn.io/status'
-    with urllib.request.urlopen(url) as response:
+    """This fetches https://alx-intranet.hbtn.io/status"""
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
         front_end = response.read()
         frontend_str = front_end.decode('utf-8')
 
-    # Print the response information
     print("Body response:")
     print("\t- type: {}".format(type(front_end)))
     print("\t- content: {}".format(front_end))
