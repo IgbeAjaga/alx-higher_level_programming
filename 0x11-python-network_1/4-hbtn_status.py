3d6bd2f9ae333b16afef8570e20984fd1abb2bcb
IgbeAjaga <ajagaigbe144@gmail.com>
2023-09-01 15:56:53 -0700

    python - network

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 """
-Fetches https://alx-intranet.hbtn.io using requests
+Fetches https://alx-intranet.hbtn.io/status using requests
 """
 import requests
 
 
 if __name__ == "__main__":
     """
-    Fetches https://alx-intranet.hbtn.io using requests
+    Fetches https://alx-intranet.hbtn.io/status using requests
     """
-    call = requests.get('https://alx-intranet.hbtn.io')
+    call = requests.get('https://alx-intranet.hbtn.io/status')
     print("Body response:")
     print("\t- type: {}".format(type(call.text)))
     print("\t- content: {}".format(call.text))
