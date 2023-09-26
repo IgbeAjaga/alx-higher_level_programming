eebf753b5c96e759a426d05f1ba5fbe8c1c42b3a
IgbeAjaga <ajagaigbe144@gmail.com>
2023-09-26 14:44:11 -0700

    update

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,31 +1,5 @@
 #!/usr/bin/node
-// Import the request module
-const request = require('request');
+// a script that gets the contents of a webpage and stores it in a file.
 const fs = require('fs');
-
-// Check if the command line arguments include a URL and a file path
-if (process.argv.length !== 4) {
-  console.error('Usage: node 5-request_store.js <URL> <file_path>');
-  process.exit(1); // Exit with an error code
-}
-
-const url = process.argv[2]; // Get the URL from the command line arguments
-const filePath = process.argv[3]; // Get the file path from the command line
-
-// Make a GET request to the specified URL
-request(url, (error, response, body) => {
-  if (error) {
-    console.error(error);
-    process.exit(1); // Exit with an error code
-  } else {
-    // Write the response body to the specified file in utf-8 encoding
-    fs.writeFile(filePath, body, 'utf-8', (err) => {
-      if (err) {
-        console.error(err);
-        process.exit(1); // Exit with an error code
-      } else {
-        console.log(`Webpage content saved to ${filePath}`);
-      }
-    });
-  }
-});
+const request = require('request');
+request(process.argv[2]).pipe(fs.createWriteStream(process.argv[3]));
