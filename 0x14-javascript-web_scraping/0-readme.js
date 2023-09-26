96b99b927b4d53e602f9ae4e142eda753ad4b1fe
IgbeAjaga <ajagaigbe144@gmail.com>
2023-09-26 14:04:13 -0700

    a script that reads and prints the content of a file

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,5 +1,23 @@
 #!/usr/bin/node
-const filepth = require('filepth');
-filepth.readFile(process.argv[2], 'utf8', function (error, content) {
-  console.log(error || content);
+// Import the fs (file system) module
+const fs = require('fs');
+
+// Check if the command line arguments include a file path
+if (process.argv.length !== 3) {
+  console.error('Usage: node 0-readme.js <file_path>');
+  process.exit(1); // Exit with an error code
+}
+
+const filePath = process.argv[2]; // Get the file path from the command line arguments
+
+// Read the content of the file in utf-8 encoding
+fs.readFile(filePath, 'utf-8', (err, data) => {
+  if (err) {
+    // Print the error object if an error occurred
+    console.error(err);
+    process.exit(1); // Exit with an error code
+  } else {
+    // Print the file content if read successfully
+    console.log(data);
+  }
 });
