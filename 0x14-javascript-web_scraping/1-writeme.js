3a2af0e834deaa9324e8ed7e6b8536f3a2e521aa
IgbeAjaga <ajagaigbe144@gmail.com>
2023-09-26 14:07:48 -0700

    a script that writes a string to a file

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,5 +1,23 @@
 #!/usr/bin/node
+// Import the fs (file system) module
 const fs = require('fs');
-fs.writeFile(process.argv[2], process.argv[3], error => {
-  if (error) console.log(error);
+
+// Check if the command line arguments include a file path and a string to write
+if (process.argv.length !== 4) {
+  console.error('Usage: node 1-writeme.js <file_path> <string_to_write>');
+  process.exit(1); // Exit with an error code
+}
+
+const filePath = process.argv[2]; // Get the file path from the command line arguments
+const stringToWrite = process.argv[3]; // Get the string to write from the command line arguments
+
+// Write the string to the file in utf-8 encoding
+fs.writeFile(filePath, stringToWrite, 'utf-8', (err) => {
+  if (err) {
+    // Print the error object if an error occurred
+    console.error(err);
+    process.exit(1); // Exit with an error code
+  } else {
+    console.log(`The string "${stringToWrite}" has been written to ${filePath}`);
+  }
 });
